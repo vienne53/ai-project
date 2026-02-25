9fdd279cd62109043c09e6a4a1279289cc0c5bb0
WANG Yingnan <viennewang53@gmail.com>
2026-02-25 19:48:20 +0800

    Initial commit

---
+++ src/.vitepress/config.ts
@@ -0,0 +1,73 @@
+import { defineConfig } from '@jcamp/vitepress-blog-theme/config'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'VitePress Blog Demo',
+  description: 'A VitePress Blog Theme',
+  themeConfig: {
+    blog: {
+      title: 'My AI Written Blog',
+      description: 'All these articles were written by AI!',
+      defaultAuthor: 'AI Writer',
+      categoryIcons: {
+        article: 'i-[heroicons-outline/book-open]',
+        tutorial: 'i-[heroicons-outline/academic-cap]',
+        document: 'i-[heroicons-outline/annotation]',
+      },
+      tagIcons: {
+        github: 'i-[carbon/logo-github]',
+        vue: 'i-[carbon/logo-vue]',
+      },
+    },
+    search: {
+      provider: 'local',
+    },
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'Theme Test', link: '/theme-test' },
+      {
+        text: 'Blog',
+        items: [
+          {
+            text: 'Blog Home',
+            link: '/blog/',
+            activeMatch: '/blog/$',
+          },
+          {
+            text: 'Tags',
+            link: '/blog/tags',
+            activeMatch: '/blog/tags',
+          },
+          {
+            text: 'Archives',
+            link: '/blog/archives',
+            activeMatch: '/blog/archives',
+          },
+          // {
+          //   text: 'RSS Feed',
+          //   link: '/blog/feed.rss',
+          // },
+        ],
+      },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/jcamp-code/vitepress-blog-theme',
+      },
+    ],
+  },
+})
